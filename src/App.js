0bb5babced54f6ee26fc599378202abf536879a4
gowtham3798 <59119223+gowtham3798@users.noreply.github.com>
2021-12-22 10:00:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,134 +2,96 @@ import "./styles.css";
 import React from 'react'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useState } from "react";
+import { useState} from "react";
 import IconButton from '@mui/material/IconButton';
-import MailIcon from '@mui/icons-material/Mail';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link,useHistory,Redirect,useParams } from "react-router-dom";
 import { MovieList } from "./MovieList";
 import { Counter } from "./Counter";
+import InfoIcon from '@mui/icons-material/Info';
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import { Color } from "./Color";
 // import { Route,BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
   const INITIAL_STATE = [
     {
-      movie: "12 Angry Men",
+        movie: "RRR",
       poster:
-        "https://i2.wp.com/wingsart.studio/wp-content/uploads/2020/11/12-Angry-Men-Landscape-Updated.jpg?resize=640%2C483&ssl=1",
-      rating: 9,
+        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+      rating: 8.8,
       summary:
-        "A dissenting juror in a murder trial slowly manages to convince the others that the case is not as obviously clear as it seemed in court."
+        "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+      trailer: "https://www.youtube.com/embed/f_vbAtFSEc0"
     },
     {
-      movie: "Avatar",
-      poster: "https://miro.medium.com/max/453/1*zWGSyuGKWgKYu-ZAf5bxiA.jpeg",
-      rating: 7.8,
-      summary:
-        "Jake, who is paraplegic, replaces his twin on the Na'vi inhabited Pandora for a corporate mission. After the natives accept him as one of their own, he must decide where his loyalties lie."
-    },
-    {
-      movie: "Adventure of Tintin",
-      poster: "https://bestsimilar.com/img/movie/thumb/af/7981.jpg",
-      rating: 7.5,
-      summary:
-        "Tintin, a young reporter, buys the model of a ship and is intrigued when his dog shows him a scroll after the toy breaks. He tells Captain Haddock and they embark on an adventure to find a shipwreck."
-    },
-    {
-      movie: "Shutter Island",
-      poster: "https://i.ytimg.com/vi/Udfq0fiScug/movieposter_en.jpg",
-      rating: 8.2,
-      summary:
-        "Teddy Daniels and Chuck Aule, two US marshals, are sent to an asylum on a remote island in order to investigate the disappearance of a patient, where Teddy uncovers a shocking truth about the place."
-    },
-
-    {
-      movie: "Iron man 2",
+        movie: "Iron man 2",
       poster:
         "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
       rating: 7,
       summary:
-        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy."
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+      trailer: "https://www.youtube.com/embed/wKtcmiifycU"
     },
     {
-      movie: "No Country for Old Men",
+        movie: "No Country for Old Men",
       poster:
         "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
       rating: 8.1,
       summary:
-        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money."
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+      trailer: "https://www.youtube.com/embed/38A__WT3-o0"
     },
     {
-      movie: "Jai Bhim",
+        movie: "Jai Bhim",
       poster:
         "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
       summary:
         "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-      rating: 8.8
+      rating: 8.8,
+      trailer: "https://www.youtube.com/embed/nnXpbTFrqXA"
     },
     {
-      movie: "The Avengers",
+        movie: "The Avengers",
       rating: 8,
-      summary: `Marvel's The Avengers (classified under the name Marvel Avengers
-      Assemble in the United Kingdom and Ireland), or simply The Avengers, is
-      a 2012 American superhero film based on the Marvel Comics superhero team
-      of the same name.`,
+      summary:
+        "Marvel's The Avengers (classified under the    movie Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same    movie.",
       poster:
-        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg"
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+      trailer: "https://www.youtube.com/embed/eOrNdBpGMv8"
     },
     {
-      movie: "Interstellar",
+        movie: "Interstellar",
       poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
       rating: 8.6,
-      summary: `When Earth becomes uninhabitable in the future, a farmer and ex-NASA
-  pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team
-  of researchers, to find a new planet for humans.`
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+      trailer: "https://www.youtube.com/embed/zSWdZVtXT7E"
     },
     {
-      movie: "Baahubali",
+        movie: "Baahubali",
       poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
       rating: 8,
-      summary: `In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.`
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+      trailer: "https://www.youtube.com/embed/sOEg_YZQsTI"
     },
     {
-      movie: "Ratatouille",
+        movie: "Ratatouille",
       poster:
         "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
       rating: 8,
-      summary: `Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.`
-    },
-{},
-    {
-      movie: 96,
-      poster:
-        "https://a10.gaanacdn.com/gn_img/albums/9En3peWXDV/En3pYMLPWX/size_xxl_1535086576.webp",
-      rating: 8.6,
-      summary: `K Ramachandran, a photographer, gets nostalgic after he visits his school in his hometown. During a reunion with his classmates, he meets Janaki, his childhood sweetheart.`
-    },
-
-    {
-      movie: "M.S. Dhoni: The Untold Story",
-      poster: "https://m.media-amazon.com/images/I/71miTEyKvYL._SL1112_.jpg",
-      rating: 7.9,
-      summary: `M S Dhoni, a boy from Ranchi, aspires to play cricket for India. Though he initially tries to please his father by working for the Indian Railways, he ultimately decides to chase his dreams.`
-    },
-    {
-      movie: "Dark Knight",
-      poster:
-        "https://i.pinimg.com/originals/0f/a9/af/0fa9afc141f0096e064a5ab1854b36f1.jpg",
-      rating: 9,
       summary:
-        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
-    },
-    {
-      movie: "King Kong",
-      poster: "https://m.media-amazon.com/images/I/817FBcXLN2L._SL1500_.jpg",
-      rating: 9,
-      summary:
-        "Peter Jackson's expansive remake of the 1933 classic follows director Carl Denham (Jack Black) and his crew on a journey from New York City to the ominous Skull Island to film a new movie. Accompanying him are playwright Jack Driscoll (Adrien Brody) and actress Ann Darrow (Naomi Watts), who is whisked away by the monstrous ape, Kong, after they reach the island. The crew encounters dinosaurs and other creatures as they race to rescue Ann, while the actress forms a bond with her simian captor."
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+      trailer: "https://www.youtube.com/embed/NgsQ8mVkN8w"
     }
   ];
+  
 
   const [movieList, setList] = useState(INITIAL_STATE);
   const [movie, setName] = useState(" ");
@@ -137,49 +99,177 @@ export default function App() {
   const [rating, setRating] = useState(" ");
   const [summary, setSummary] = useState(" ");
   console.log([...movieList]);
+  const history = useHistory();
 
+  var arr=[]
+
+  console.log(movie)
   return (
     
     <div className="App">
+      
         <h1 className="hea">MOVIES</h1>
 <div>
-  <nav>
-<Link to="home">Home</Link>
-<Link to="/movies">Movies</Link>
-<Link to="/counter">Count</Link>
-<Link to="/newform">Add Movie</Link>
+<AppBar position="static">
+ <nav>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("/home")}>
+    Home
+  </Button>
+</Toolbar>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("/movies")}>
+    Movies
+  </Button>
+</Toolbar>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("/films")}>
+    Films
+  </Button>
+</Toolbar>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("/counter")}>
+    Counter
+  </Button>
+</Toolbar>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("/newform")}>
+    Add
+  </Button>
+</Toolbar>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("/tictac")}>
+    TIC-TAC-TOE
+  </Button>
+</Toolbar>
+<Toolbar>
+  <Button color="inherit" onClick={() => history.push("**")}>
+    Not found
+     </Button>
+</Toolbar>
 </nav>
+</AppBar>
 <hr />
 <div>
   <Switch>
+  
   <Route path="/home">
     <Home />
-  </Route></Switch>
-</div>
-<Switch>
+  </Route>
 
       <Route path="/counter">
           <Counter />
         </Route>
-</Switch>
-</div>
-
-
-
-<div>
-     <Route path="/newform">
-    {addmovie()}
+   
+    <Route path="/newform">
+    <Addmovie movies={movieList} setList={setList}/>
       </Route>
-      </div>
-  
-      <Route path="/movies">
+   
+     <Route path="/movies">
       <MovieList movies={movieList} setList={setList} /> 
      </Route>
-     
+
+     <Route path="/films">
+      <Redirect to="/movies"/> 
+     </Route>
+   
+      <Route path="/details/:id">
+        <MovieDetails movies={movieList} />
+      </Route>
+
+      <Route path="/tictac">
+      <TicTacToe />
+     </Route>
+      
+      <Route path="**">
+      <NotFound />
+     </Route>
+
+   
+     </Switch>
+    </div>
+    </div>
     </div>
   );
 
-  function addmovie() {
+
+  function TicTacToe() {
+    const{width,height} = useWindowSize();
+    const[board,setBoard] = useState([null,null,null,null,null,null,null,null,null]);
+
+    const[isXTurn,setXTurn] = useState(true);
+
+    const handleClick = (index) => {
+      console.log("Clicked",index);
+
+      if(!winner && !board[index]){
+        const boardCopy = [...board];
+        boardCopy[index] = isXTurn ? "X" : "O";
+        setBoard(boardCopy);
+        setXTurn(!isXTurn);
+        console.log(index)
+      }
+    }
+    
+
+      const decideWinner = (board) => {
+        const lines =[
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+          [0,4,8],
+          [2,4,6],
+        ];
+      
+      for(let i=0;i<lines.length;i++){
+const[a,b,c] = lines[i];
+console.log(lines[i])
+if(board[a] !== null && board[a] === board[b] && board[a]===board[c]){
+  console.log("Winner is", board[a]);
+  return(board[a])
+}
+      }
+      return null;
+      }
+      const winner = decideWinner(board);
+    
+    return(
+      <div className="tictac">
+     
+      <div className="gameboard">
+    {board.map((val, index) => (
+      <GameBox val={val} onPlayerClick={()=> handleClick(index)}/>))}
+      <div className="restart">
+       <Button onClick={() => window.location.reload(false)}><IconButton aria-label="delete" color="primary"><RestartAltIcon />
+          </IconButton>Restart</Button></div>
+    </div>
+   
+    {winner ?   <p>The Winner is {winner} <Confetti
+      width={width}
+      height={height}
+      friction={0.99}
+    ></Confetti></p> :" "}
+    
+    
+    
+    </div>
+    )
+  }
+
+ 
+
+  function GameBox({val , onPlayerClick}){
+  //  const [val,setVal] = useState(null)
+    const a = { color: val==="X" ? "red" : "blue" };
+    return(
+      <div style={a} className="gamebox" onClick={() => onPlayerClick()}>{val}</div>
+    )
+  }
+
+  function Addmovie() {
     return <div className="add-movie-form">
       <TextField id="standard-basic" label="movie title" variant="standard"
         value={movie}
@@ -202,8 +292,9 @@ export default function App() {
             poster,
             rating,
             summary
-          };
+          }
           setList([...movieList, newMovie]);
+          history.push("/movies");
         } }
       >
         UPLOAD
@@ -212,16 +303,11 @@ export default function App() {
   }
 }
 
-function Home(){
-  return(
-    "Welcome to the movie collection"
-  );
-}
-
-
-export function Movie({ movie, poster, rating, summary, deleteButton }) {
+export function Movie({ movie, poster, rating, summary, deleteButton,id}) {
   const [show, setShow] = useState(false);
   const a = { display: show ? "block" : "none" };
+  const history = useHistory();
+  // const nhis = useHistory()
   return (
     <div>
       <div>
@@ -229,11 +315,18 @@ export function Movie({ movie, poster, rating, summary, deleteButton }) {
           <img src={poster} alt="" className="movie-img" />
           <div className="movie-title">
             <h2>{movie}</h2>
+         
             <p>
               <Color rating={rating} />
             </p>
           </div>
-          <IconButton aria-label="delete" onClick={() => setShow(!show)}>{!show ?<ExpandMoreIcon/>: <ExpandLessIcon />}</IconButton>
+          <IconButton aria-label="delete"
+           onClick={() => (history.push(`/details/${id}`))}
+        ><InfoIcon />
+          </IconButton>
+          
+          <IconButton aria-label="delete" onClick={() => setShow(!show)}>{!show ?<ExpandMoreIcon/>: <ExpandLessIcon />}
+          </IconButton>
           <p style={a} className="movie-summary">
             {summary}
           </p>
@@ -249,25 +342,62 @@ export function Movie({ movie, poster, rating, summary, deleteButton }) {
   );
 }
 
-function Color({ rating }) {
-  const a = { color: "green", fontSize: "13px" };
-  const b = { color: "red", fontSize: "13px" };
-  return rating >= 8.5 ? (
-    <p style={a}>
-      {" "}
-      <span role="img" aria-label="emo">
-        ⭐
-      </span>
-      {rating}
-    </p>
-  ) : (
-    <p style={b}>
-      {" "}
-      <span role="img" aria-label="emo">
-        ⭐
-      </span>
-      {rating}
-    </p>
+function MovieDetails({movies}){
+  
+  
+  const {id} = useParams()
+  const movieinfo = movies[id];
+  console.log(movies[id])
+//  const {img} = useParams()
+  return(
+    <div> <div className="info-container">
+        {/* <img src={movieinfo.poster } alt="" className="movie-img" /> */}
+        <iframe width="942" height="530" src={movieinfo.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div className="movie-title">
+          <h2>{movieinfo.movie}</h2>
+        <p>
+            <Color rating={movieinfo.rating} />
+          </p></div>
+          <p className="movie-summary">
+        {movieinfo.summary} </p>
+          
+        </div>
+          </div>
+       
+);
+}
+
+function NotFound(){
+  return(
+    <div>
+  <h2>404</h2>
+  <img src="https://iqonic.design/wp-content/uploads/2021/06/0_uqbx_RV-7Vzz1Yc8.gif" />
+  </div>
   );
 }
 
+function Home(){
+ const styles = {fontSize : "40px" , textAlign : "center"}
+  return(
+    <div style={styles}>Welcome to the movie collection</div>
+  );
+}
+
+
+// {movies.map(({ movie, poster, rating, summary }, index) => (
+//   <Movie infoMovie={<IconButton aria-label="delete" color="error" size="small"
+//   onClick={() => {
+//     const infoindex = index;
+//     const remaininginfo = movies.reduce(
+//       (mv, ind) => infoindex == ind
+//     );
+//     console.log(movies, remaininginfo);
+//     setList(remaininginfo);
+//   }}
+// ><InfoIcon />
+//  </IconButton>}
+//  id={index}
+//  poster={poster}
+//  movie={movie}
+//  rating={rating}
+//  summary={summary} />))}
